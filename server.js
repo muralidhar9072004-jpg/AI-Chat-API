7c17cb61d83521c75ee9886a0d00e515da52561a
Muralidhar M <product5@mooving.com>
2026-04-23 12:19:20 +0530

    Backend ready

---
+++ server.js
@@ -0,0 +1,147 @@
+import express from "express";
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import mongoose, { model } from "mongoose";
+import cors from "cors";
+
+
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//mongoose.connect("mongodb://localhost:27017/chatdb")
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("mongo connected"))
+    .catch(err => console.log(err));
+
+
+
+const sessionSchema = new mongoose.Schema({
+    userId: String,
+    history: [String],
+    summary: String,
+    message: String,
+    createdAt: { type: Date, default: Date.now }
+})
+
+const Session = mongoose.model("Session", sessionSchema);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function updatesummary(usersessions) {
+    const model = genAI.getGenerativeModel({
+        model: "gemini-2.5-flash-lite",
+    });
+
+    const prompt =
+        `
+    you are a summarizer.
+
+    current summary:${usersessions.summary}.
+
+    recent conversation:
+    ${usersessions.history.slice(-6).join("\n")}
+    update the summary.
+    keep important topics,goals,decisions,preferences.
+    keep it concise.
+
+    new summary:
+    `;
+
+
+    const result = await model.generateContent(prompt);
+    return await result.response.text();
+}
+
+
+app.post("/chat", async (req, res) => {
+    try {
+        const { userId, message } = req.body;
+        let usersessions = await Session.findOne({ userId });
+
+        if (!usersessions) {
+
+            usersessions = new Session({
+                userId,
+                history: [],
+                summary: ""
+            })
+        }
+        if (usersessions.history.length % 6 === 0) {
+            usersessions.summary = await updatesummary(usersessions);
+        }
+
+        usersessions.history.push(`User:${message}`);
+
+
+        const prompt =
+            `you are a helpful assistant. 
+        
+        summary :
+        ${usersessions.summary}
+        
+        Recent conversation :
+        ${usersessions.history.slice(-4).join("\n")}
+        
+        Ai:
+        `;
+
+
+
+        const model = genAI.getGenerativeModel({
+            model: "gemini-2.5-flash-lite",
+
+        });
+
+
+        const result = await model.generateContent(prompt);
+        const reply = await result.response.text();
+        usersessions.history.push(`Ai:${reply}`);
+        await usersessions.save();
+
+        res.json({ reply })
+    }
+    catch (error) {
+        console.error("\n[AI Error]:", error);
+        res.status(500).json({ error: "Failed to get response from AI", details: error.message });
+    }
+});
+
+app.post("/clear-chat", async (req, res) => {
+    try {
+        const { userId } = req.body;
+        await Session.deleteOne({ userId });
+        res.json({ message: "chat cleared" });
+
+    }
+    catch (error) {
+        res.status(500).json({ error: error.message });
+
+    }
+
+});
+
+app.get("/history/:userId", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const usersessions = await Session.findOne({ userId });
+
+        if (!usersessions) {
+            return res.json({
+                history: [],
+                summary: ""
+            });
+        }
+        res.json({
+            history: usersessions.history,
+            summary: usersessions.summary
+        })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
